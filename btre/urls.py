2d29d60b3b5c2fb1b573ab6579b26b7303eb1bc3
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2020-10-10 11:06:07 -0700

    Adding media folder and home images

--- btre/urls.py
+++ btre/urls.py
@@ -4,9 +4,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+# Add below to support "static(settings..."
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')), # linking to the urls.py of the pages app
     path('listings/', include('listings.urls')), # linking to the urls.py of the pages app
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Add this to have access to images
