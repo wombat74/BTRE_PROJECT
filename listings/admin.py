dc42f408df2cca375bc753f073dfc4cb254496c3
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2020-10-11 15:40:12 -0700

    Added pagination to listings

--- listings/admin.py
+++ listings/admin.py
@@ -6,7 +6,7 @@ class ListingAdmin(admin.ModelAdmin): # This allows us to customize the admin pa
     list_display_links = ('id', 'title') # This enables the id and title to be hyperlinks
     list_filter = ('realtor',) # This makes any field a filter
     list_editable = ('is_published',) # This makes a field editable
-    search_fields = ('title', 'desctiption', 'address', 'city', 'state', 'zipcode', 'price')
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
     list_per_page = 25
     
 admin.site.register(Listing, ListingAdmin) # This setting hooks the Listing model to the Admin app
--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 def index(request):
-    listings = Listing.objects.all()
+    # Grab all of the listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True) # "-" sets it as descending
+
+    paginator = Paginator(listings, 6) # number of items per page
+    page = request.GET.get('page') # grab the "page" url parameter
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render(request, 'listings/listings.html', context)
 
-def listing(request):
+def listing(request, listing_id): # must add listing_id so that we can reference the id
     return render(request, 'listings/listing.html')
 
 def search(request):
