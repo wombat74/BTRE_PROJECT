9962bfe42e02e1df0433cb78a67fec0b6afba363
Eddie Gonzalez <eddie.gonzalez@gmail.com>
2020-10-10 19:18:00 -0700

    Adding updates to Admin page

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Listing
 
-# This setting hooks the Listing model to the Admin app
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin): # This allows us to customize the admin page
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor') # This adds fields to the table
+    list_display_links = ('id', 'title') # This enables the id and title to be hyperlinks
+    list_filter = ('realtor',) # This makes any field a filter
+    list_editable = ('is_published',) # This makes a field editable
+    search_fields = ('title', 'desctiption', 'address', 'city', 'state', 'zipcode', 'price')
+
+admin.site.register(Listing, ListingAdmin) # This setting hooks the Listing model to the Admin app
 
 
